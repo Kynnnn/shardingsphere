df0b6bb86bd1f03596b96b4d5e52326a50cc4b0c
zhudiwei_13zhfx@126.com <zhudiwei_13zhfx@126.com>
2020-09-13 12:11:14 +0800

    'devCommit'

---
+++ src/main/java/com/kynnnn/shardingsphere/com/kynnnn/shardingsphere/model/UserDetail.java
@@ -0,0 +1,16 @@
+package com.kynnnn.shardingsphere.com.kynnnn.shardingsphere.model;
+
+import lombok.Data;
+
+/**
+ * @author Zhudiwei
+ */
+@Data
+public class UserDetail {
+
+    private String id;
+
+    private String name;
+
+
+}
